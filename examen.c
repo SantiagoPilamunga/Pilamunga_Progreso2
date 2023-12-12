4070065addb84550a3daa3a71849f98ecbe88377
Santiago <santiago.pilamunga@udla.edu.ec>
2023-12-12 14:09:30 -0500

    Se imprimen los valores tomando en cuenta que las columnas son los ingresos y egresos y las filas los insumos

--- examen.c
+++ examen.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main()
 {
     int l,n,m;
     l=12;
-    n=2;
-    m=30;
+    n=30;
+    m=2;
+    srand(time(NULL));
     int matrix[l][n][m];
     for (int k = 0; k < l; k++)
     {
@@ -12,12 +15,29 @@ int main()
         {
             for (int j = 0; j < m; j++)
             {
-                
+                matrix[k][i][j]=rand()%101;
             }
             
         }
         
     }
-    
+    mostrar (l,n,m,matrix);
     return 0;
+}
+void mostrar (int l, int n, int m,int matriz[l][n][m])
+{
+    for (int k = 0; k < l; k++)
+    {
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                printf("%d \t", matriz[k][i][j]);
+            }
+            printf("\n");
+        }
+        
+    }
+    
 }
