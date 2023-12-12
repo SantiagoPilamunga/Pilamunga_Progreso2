09cdfba096c9305f98f00bf8d917b0884618fed8
Santiago <santiago.pilamunga@udla.edu.ec>
2023-12-12 13:56:05 -0500

    Creamos un arreglo

--- examen.c
+++ examen.c
@@ -6,5 +6,18 @@ int main()
     n=2;
     m=30;
     int matrix[l][n][m];
+    for (int k = 0; k < l; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                
+            }
+            
+        }
+        
+    }
+    
     return 0;
 }
